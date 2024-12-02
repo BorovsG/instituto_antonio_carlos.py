417cd62f61a6e1c24018b37e77454c593a35d482
Borovs <gabrielborovs@hotmail.com>
2024-12-02 02:30:13 -0300

    Update instituto_antonio_carlos.py

--- instituto_antonio_carlos.py
+++ instituto_antonio_carlos.py
@@ -186,6 +186,7 @@ def excluir_cadastro_view():
                 st.error("CPF não encontrado.")
         else:
             st.error("CPF inválido. Deve conter apenas números e ter 11 dígitos.")
+
 def exibir_cursos():
     st.header("Cursos Disponíveis")
     cursos = {
